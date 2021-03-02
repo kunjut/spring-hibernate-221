4718061d7a0a7bfb5f7051d221f94b22e929df4a
kunjut <v.dimnich@gmail.com>
2021-03-03 02:38:40 +0300

    Add user + car to db, get them, get user by car

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,23 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      //      Создается несколько пользователей с машинами,
+      //      добавляются в базу данных
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      user1.setUserCar(new Car("BMW", 3));
+      userService.add(user1);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      user2.setUserCar(new Car("ОКА", 1111));
+      userService.add(user2);
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      user3.setUserCar(new Car("ВАЗ", 2106));
+      userService.add(user3);
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+      user4.setUserCar(new Car("ГАЗ", 31105));
+      userService.add(user4);
+
+      //      Вынимаются обратно
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +40,17 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getUserCar());
          System.out.println();
       }
+
+      // В сервис был добавлен метод, который с помощью hql-запроса достает юзера,
+      // владеющего машиной по ее модели и серии.
+
+      System.out.println(userService.userByCar("ГАЗ", 31105) + "\n");
+      System.out.println(userService.userByCar("ВАЗ", 2106) + "\n");
+      System.out.println(userService.userByCar("ОКА", 1111) + "\n");
+      System.out.println(userService.userByCar("BMW", 3));
 
       context.close();
    }
